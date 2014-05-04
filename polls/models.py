16194aea0f4452ad04f945e672e0ab34aab642f9
sheeksav <andy.sheeks@gmail.com>
2014-05-04 15:18:58 -0400

    Added some comments to models.py

--- polls/models.py
+++ polls/models.py
@@ -7,12 +7,14 @@ class Poll(models.Model):
 	question = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+	#returns unicode string of question name
 	def __unicode__(self):
 		return self.question
 
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 	
+	# These are display functions for admin backend
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
