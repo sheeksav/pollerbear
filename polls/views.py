3606700c7137adda1cc0d8f1d238462a6338506e
sheeksav <andy.sheeks@gmail.com>
2014-05-04 10:00:55 -0400

    Modified index view to use render() shortcut function.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from polls.models import Poll
 
@@ -7,11 +7,8 @@ from polls.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		'latest_poll_list': latest_poll_list,
-	})
-	return HttpResponse(template.render(context))
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
