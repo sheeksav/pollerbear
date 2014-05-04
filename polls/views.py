4d7e69cba4cde7b7954405a854dc760ce8ab57fe
sheeksav <andy.sheeks@gmail.com>
2014-05-04 15:16:30 -0400

    A shortcut way to raise 404 exception.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll
 
@@ -13,11 +12,8 @@ def index(request):
 
 
 def detail(request, poll_id):
-    try:
-    	poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-    	raise Http404
-    return render(request, 'polls/detail.html', {'poll': poll})
+   poll = get_object_or_404(Poll, pk=poll_id)
+   return render(request, 'polls/detail.html', {'poll': poll})
 
 
 def results(request, poll_id):
