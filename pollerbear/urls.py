7ef13e42f2c0b2d6f6273100c991f72acd713e8f
sheeksav <andy.sheeks@gmail.com>
2014-05-05 16:06:03 -0400

    Created 'generic' app, homepage view. Uploaded Bootstrap.

--- pollerbear/urls.py
+++ pollerbear/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from generic.views import HomeView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', HomeView.as_view(), name='home') # URLconf for homepage
 )
